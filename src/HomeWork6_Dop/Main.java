dce18885e1cc5b30d3bc23e9887b036e6b1610db
anzelikalevonyuk <anzhelika.levonyuk@gmail.com>
2024-03-21 12:16:41 +0300

    HomeWork6

--- src/HomeWork6_Dop/Main.java
+++ src/HomeWork6_Dop/Main.java
@@ -6,32 +6,33 @@ public class Main {
         Patient patient1 = new Patient();
         patient1.setTreatmentPlan(new TreatmentPlan(1));
         setDoc(patient1);
+        patient1.doctor.heal();
+
 
         Patient patient2 = new Patient();
         patient2.setTreatmentPlan(new TreatmentPlan(2));
         setDoc(patient2);
+        patient2.doctor.heal();
 
         Patient patient3 = new Patient();
         patient3.setTreatmentPlan(new TreatmentPlan(3));
         setDoc(patient3);
+        patient3.doctor.heal();
 
     }
 
     public static void setDoc (Patient patient){
         if (patient.getTreatmentPlan().getCode() == 1)
         {
-            patient.setDoctor(new Surgeon());
-            new Surgeon().heal();
+            patient.setDoctor(patient.doctor = new Surgeon());
         }
         else if (patient.getTreatmentPlan().getCode() == 2)
         {
-            patient.setDoctor(new Dentist());
-            new Dentist().heal();
+            patient.setDoctor(patient.doctor = new Dentist());
         }
         else
         {
-            patient.setDoctor(new Therapist());
-            new Therapist().heal();
+            patient.setDoctor(patient.doctor = new Therapist());
         }
     }
 }
